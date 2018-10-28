09d500b563dd38d1d74502d6e123ab67d92e6f1d
Andrew Bms <abergmanson@gmail.com>
2018-10-28 00:47:47 -0400

    added login/logout

---
+++ project/_config.py
@@ -0,0 +1,14 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'password'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = '\x9b\xaf\xe5\xb2-h\xab\xeb\xd67\xec`\x80\x0b]\xd7Tt\xb5c\xf3pk\xdf'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
